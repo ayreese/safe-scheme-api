64a78528df5395a9ae1c5657b8a6427860f923da
Alex <alexis@alexisyemil.io>
2024-12-09 22:44:56 -0600

    Removed incomplete delete-project.mjs function

--- src/handlers/delete-project.mjs
+++
@@ -1,8 +0,0 @@
-import { validateBody } from "../../functions/validate.mjs";
-
-
-// Function to get projects from table based on user-id
-export const deleteProjectHandler = async (event) => {
-    await validateBody(event);
-
-};
