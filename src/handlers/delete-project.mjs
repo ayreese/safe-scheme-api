e0d4869f56e220c702d085c0b71ef40c232b037d
Alex <alexis@alexisyemil.io>
2025-01-06 23:32:24 -0600

    Added OpenAPI definition and Lambda function for delete-project endpoint

---
+++ src/handlers/delete-project.mjs
@@ -0,0 +1,36 @@
+import {client} from "../../utils/dynamoClient.mjs";
+import {DeleteCommand} from "@aws-sdk/lib-dynamodb";
+
+export const deleteProjectHandler = async (event) => {
+    const tableName = process.env.PROJECTS_TABLE;
+    const userId = event.pathParameters.UserId
+    const projectId = event.pathParameters.ProjectId
+
+    try {
+        await client.send(new DeleteCommand({
+            TableName: tableName,
+            Key: {
+                UserId: userId,
+                ProjectId: projectId,
+            },
+            ConditionExpression: "ProjectId = :projectId",
+            ExpressionAttributeValues: {
+                ":projectId": projectId,
+            }
+        }))
+        return {
+            statusCode: 204,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (error) {
+        console.log("ERROR:", error);
+        return {
+            statusCode: 404,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    }
+};
