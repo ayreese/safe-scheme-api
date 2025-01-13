d24a71392b4e951c0d1dd459b9fb37b5ac6b34fe
Alex <alexis@alexisyemil.io>
2025-01-13 02:27:06 -0600

    Changed condition expression and update params

--- src/handlers/edit-task.mjs
+++ src/handlers/edit-task.mjs
@@ -3,28 +3,38 @@ import {UpdateCommand} from "@aws-sdk/lib-dynamodb";
 // import {tasksHelper} from "../../functions/tasks-helper.mjs";
 
 export const editTaskHandler = async (event) => {
-    const task = JSON.parse(event.body);
-    console.log("THIS IS THE TASK", task);
-    const {taskId, name, status} = task;
+    const taskParameters = JSON.parse(event.body);
+    console.log("THIS IS THE TASK", taskParameters);
+    const {Task: task, Status: status} = taskParameters;
     const tableName = process.env.PROJECTS_TABLE;
     const projectId = event.pathParameters.ProjectId;
-    // const taskId = event.pathParameters.ProjectId;
+    const taskId = event.pathParameters.TaskId;
 
     try {
         const data = await client.send(new UpdateCommand({
             TableName: tableName,
             Key: {
-                UserId: "e1df74f8610de4bd",
+                UserId: "3aa2a3ab-3e4c-4c11-a774-82fa1f27bb6a",
                 ProjectId: projectId,
             },
-            ConditionExpression: "taskId = :taskId",
-            UpdateExpression: "SET name = :name, status = :status",
+            ConditionExpression: 'attribute_exists(Task) AND TaskID = :taskId',
+            UpdateExpression: 'SET #status = :status, Task = :task',
+            ExpressionAttributeNames: {
+                '#status': 'Status'  // Use a placeholder for the reserved word
+            },
             ExpressionAttributeValues: {
-                ":taskId": taskId,
-                ":name": name,
-                ":status": status,
-
+                ':taskId': taskId,
+                ':task': task,
+                ':status': status
             }
+            // ConditionExpression: "TaskId = :taskId",
+            // UpdateExpression: "SET Task = :task, Status = :status",
+            // ExpressionAttributeValues: {
+            //     ":taskId": taskId,
+            //     ":task": task,
+            //     ":status": status,
+            //
+            // }
         }))
         return {
             statusCode: 200,
