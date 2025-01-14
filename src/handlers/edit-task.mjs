c136c884c1cdb1cec27dafde1875f3d150e87e4d
Alex <alexis@alexisyemil.io>
2025-01-14 01:45:58 -0600

    Updated edit-task.mjs function

--- src/handlers/edit-task.mjs
+++ src/handlers/edit-task.mjs
@@ -4,37 +4,30 @@ import {UpdateCommand} from "@aws-sdk/lib-dynamodb";
 
 export const editTaskHandler = async (event) => {
     const taskParameters = JSON.parse(event.body);
-    console.log("THIS IS THE TASK", taskParameters);
-    const {Task: task, Status: status} = taskParameters;
+    const {Description: description, Status: status} = taskParameters;
     const tableName = process.env.PROJECTS_TABLE;
     const projectId = event.pathParameters.ProjectId;
     const taskId = event.pathParameters.TaskId;
+    const userId = event.requestContext.authorizer.claims.sub;
 
     try {
+        console.log("Task ID is of type", typeof taskId)
         const data = await client.send(new UpdateCommand({
             TableName: tableName,
             Key: {
-                UserId: "3aa2a3ab-3e4c-4c11-a774-82fa1f27bb6a",
+                UserId: userId,
                 ProjectId: projectId,
             },
-            ConditionExpression: 'attribute_exists(Task) AND TaskID = :taskId',
-            UpdateExpression: 'SET #status = :status, Task = :task',
+            // ConditionExpression: 'Tasks.taskId = :taskId',
+            UpdateExpression: 'SET Tasks.#taskId.Description = :Description, Tasks.#taskId.#Status = :Status',
             ExpressionAttributeNames: {
-                '#status': 'Status'  // Use a placeholder for the reserved word
+                "#taskId": taskId,
+                "#Status": "Status",
             },
             ExpressionAttributeValues: {
-                ':taskId': taskId,
-                ':task': task,
-                ':status': status
+                ":Description": description,
+                ":Status": status
             }
-            // ConditionExpression: "TaskId = :taskId",
-            // UpdateExpression: "SET Task = :task, Status = :status",
-            // ExpressionAttributeValues: {
-            //     ":taskId": taskId,
-            //     ":task": task,
-            //     ":status": status,
-            //
-            // }
         }))
         return {
             statusCode: 200,
