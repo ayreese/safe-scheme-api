456b276b87496e7217b40868ca07d70fab1ddaaa
Alex <alexis@alexisyemil.io>
2025-01-15 12:31:26 -0600

    Updated dynamoClient.mjs

--- utils/dynamoClient.mjs
+++ utils/dynamoClient.mjs
@@ -1,7 +1,6 @@
 import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
 import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
 
-
 const config = new DynamoDBClient({
     region: "us-east-1",
     logger: console,
