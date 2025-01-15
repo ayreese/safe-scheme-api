f477036cc4485b92400ebc2b09753e5149dc7ee0
Alex <alexis@alexisyemil.io>
2025-01-15 01:16:58 -0600

    Updated comments and removed test userId

--- src/handlers/get-projects.mjs
+++ src/handlers/get-projects.mjs
@@ -1,15 +1,9 @@
 import {client} from "../../utils/dynamoClient.mjs";
 import {QueryCommand} from "@aws-sdk/lib-dynamodb";
 
-// Function to get projects from table based on UserId
 export const getProjectsHandler = async (event) => {
-    /* Testing UserID */
-    const userId = event.pathParameters.UserId;
-    /* Production UserID */
-    // const userId = event.requestContext.authorizer.principalId;
-
-
     const table = process.env.PROJECTS_TABLE;  // Get table name from environment variables
+    const userId = event.requestContext.authorizer.claims.sub; // Get userId from event provided by cognito
 
     try {
         // Query DynamoDB for user data
@@ -20,24 +14,22 @@ export const getProjectsHandler = async (event) => {
                 ":userId": userId,
             },
         }));
-        console.log("ALL TABLE ITEMS",Items);
-
         // Switch based on Items
         switch (true) {
+            // Found items and return to user
             case (Items && Items.length > 0):
-                console.info('Found projects:', Items);
                 return {
                     statusCode: 200,
                     body: JSON.stringify({projects: Items}),
                 };
-
+            // No items found, returns 404 not found
             case (Items && Items.length === 0):
                 console.warn('No projects found for UserId:', userId);
                 return {
                     statusCode: 404,
                     body: JSON.stringify({message: "Not Found"}),
                 };
-
+            // Default statement for all other errors
             default:
                 console.error('Unexpected error or condition occurred');
                 return {
@@ -46,7 +38,6 @@ export const getProjectsHandler = async (event) => {
                 };
         }
     } catch (error) {
-        // Log error and return the error response
         console.error('Error occurred while querying DynamoDB:', error.message);
         return {
             statusCode: 500,
