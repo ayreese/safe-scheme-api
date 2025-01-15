a8f9eb3861786b9e5e47d5d542510611f5d52e16
Alex <alexis@alexisyemil.io>
2025-01-15 00:02:10 -0600

    Added create-subtask.mjs and added function to template.yaml

---
+++ src/handlers/create-subtask.mjs
@@ -0,0 +1,51 @@
+/*
+To create a userID to test function use:
+const userId = crypto.randomBytes(8).toString('hex');
+*/
+
+import {client} from "../../utils/dynamoClient.mjs";
+import crypto from 'crypto';
+import {UpdateCommand} from "@aws-sdk/lib-dynamodb";
+
+export const createSubtaskHandler = async (event) => {
+    const tableName = process.env.PROJECTS_TABLE; // DynamoDB table in SAM template
+    const subtask = JSON.parse(event.body);
+    const {Subtask: description, Status: status} = subtask;
+    const userId = event.requestContext.authorizer.claims.sub; // Get userId from event provided by cognito
+    const projectId = event.pathParameters.ProjectId;
+    const taskId = event.pathParameters.TaskId;
+    const subtaskId = crypto.randomUUID()
+
+    try {
+        await client.send(new UpdateCommand({
+            TableName: tableName,
+            Key: {
+                UserId: userId,
+                ProjectId: projectId,
+            },
+            ConditionExpression: "attribute_exists(Tasks.#TaskId)",
+            UpdateExpression: "SET Tasks.#TaskId.Subtasks.#SubtaskId = :subtaskData",
+            ExpressionAttributeNames: {
+                "#TaskId": taskId,
+                "#SubtaskId": subtaskId,
+            },
+            ExpressionAttributeValues: {
+                ":subtaskData": {
+                    "Description": description,
+                    ":Status": status,
+                }
+            },
+        }));
+        console.info(`Successfully created subtask: ${description}`);
+        return {
+            statusCode: 201,
+            body: JSON.stringify({message: "Created subtask"})
+        };
+    } catch (e) {
+        console.error("Error creating subtask", e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: "Failed to create subtask", error: e.message})
+        };
+    }
+};
