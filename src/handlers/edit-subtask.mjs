040e5c6e8cccb09421f8a98c55f86b85d24aecf6
Alex <alexis@alexisyemil.io>
2025-01-15 12:27:16 -0600

    Updated edit-subtask.mjs

---
+++ src/handlers/edit-subtask.mjs
@@ -0,0 +1,43 @@
+import {client} from "../../utils/dynamoClient.mjs";
+import {UpdateCommand} from "@aws-sdk/lib-dynamodb";
+
+export const editSubtaskHandler = async (event) => {
+    const tableName = process.env.PROJECTS_TABLE;
+    const userId = event.requestContext.authorizer.claims.sub;
+    const subtaskParameters = JSON.parse(event.body);
+    const {Description: description, Status: status} = subtaskParameters;
+    const projectId = event.pathParameters.ProjectId;
+    const taskId = event.pathParameters.TaskId;
+    const subtaskId = event.pathParameters.SubtaskId;
+
+
+    try {
+        const data = await client.send(new UpdateCommand({
+            TableName: tableName,
+            Key: {
+                UserId: userId,
+                ProjectId: projectId,
+            },
+            UpdateExpression: 'SET Tasks.#TaskId.Subtask.#SubtaskId.Description = :Description, Tasks.#TaskId.Subtask.#SubtaskId.#Status = :Status',
+            ExpressionAttributeNames: {
+                "#TaskId": taskId,
+                "#SubtaskId": subtaskId,
+                "#Status": "Status",
+            },
+            ExpressionAttributeValues: {
+                ":Description": description,
+                ":Status": status
+            }
+        }))
+        return {
+            statusCode: 200,
+            body: JSON.stringify({data: data})
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: `Task ${subtaskId} could not be updated`})
+        }
+
+    }
+};
