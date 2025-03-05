ee2d35a351ccf9ee20a68cd3d49cf0c8dadca4ac
Alex <alexis@alexisyemil.io>
2025-03-05 04:27:01 -0600

    Updated headers

--- utils/headers.ts
+++ utils/headers.ts
@@ -12,5 +12,6 @@ export interface LambdaResponse {
 export const responseHeaders = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With, Accept",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
 };
