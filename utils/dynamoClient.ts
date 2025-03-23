4959710b430c5557c683f44f716b51830e556830
Alex <alexis@alexisyemil.io>
2025-03-23 06:03:55 -0500

    Updated DynamoDB table

--- utils/dynamoClient.ts
+++ utils/dynamoClient.ts
@@ -4,7 +4,6 @@ import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
 const config: DynamoDBClient = new DynamoDBClient({
     region: "us-east-1",
     logger: console,
-    endpoint: "http://host.docker.internal:8000"
 });
 
 export const client: DynamoDBDocumentClient = DynamoDBDocumentClient.from(config);
